7a095c1827571970c4223a3ddbfaa0928261b2d3
Lovisa <lovisa.bohman@se.experis.com>
2023-01-26 14:48:00 +0100

    Create higher order component for handling authentication of user.

---
+++ src/hoc/withAuth.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useUser } from "../context/UserContext"
+
+// Uses the closure syntax, a function that returns another function, 
+// which uses the first/outer functions arguments in the inner function
+const withAuth = Component => props => {
+    const { user } = useUser();
+    if (user !== null) {
+        return <Component {...props} />
+    } else {
+        return <Navigate to="/" />
+    }
+}
+export default withAuth;
